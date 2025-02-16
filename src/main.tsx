d8d25ae26fd1fe789dab0b9ad9133c4d34c215ee
Raina Moon <mds6425@gmail.com>
2025-02-17 03:31:40 +0900

    ✨ Add Sentry error tracking and monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
+import Sentry from "./utils/sentry.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Sentry.ErrorBoundary fallback={<p>⚠️ 오류가 발생했습니다!</p>}>
+        <App />
+      </Sentry.ErrorBoundary>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
---
+++ src/utils/sentry.ts
@@ -0,0 +1,18 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://3e37339c7a0dcda520c58dcb7dd677b5@o4508830281105408.ingest.us.sentry.io/4508830283005952",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+export default Sentry;
